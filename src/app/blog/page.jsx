3d6edc1826f8fafdd19e3d6c257224569902cc09
HaileyPong <haileypong@berkeley.edu>
2023-07-20 15:28:40 -0400

    added directories

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import React from 'react'
+
+export default function Blog() {
+  return (
+    <div>Blog</div>
+  )
+}
---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>HOME</div>
+  )
+}
